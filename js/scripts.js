e5878a21bb86567442734bc70b94e5cf21365625
victormongare1 <vicmongz254@gmail.com>
2020-02-17 09:27:58 +0300

    indentation

--- js/scripts.js
+++ js/scripts.js
@@ -11,57 +11,57 @@ function checkgender() {
   var boy = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   var girl = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
   if (document.getElementById("male").checked){
-  var gender = 'male';
-  }
+    var gender = 'male';
+    }
   else if(document.getElementById("female").checked){
-  var gender= 'female';
-  }
+    var gender= 'female';
+    }
   if(mm < 1 || mm > 12 || mm == 2 && dd > 29) {
-  alert("INVALID MONTH");
-  }
+    alert("INVALID MONTH");
+    }
   else if(dd < 1 || dd > 31) {
-  alert("INVALID DAY");
-  }
+    alert("INVALID DAY");
+    }
   else if(Math.ceil(result) === 1 && gender === 'male'){
-  alert("Born on Monday ,your akan name is " + boy[1]);
+    alert("Born on Monday ,your akan name is " + boy[1]);
     }
   else if(Math.ceil(result) === 2 && gender === 'male') {  
-  alert("Born on Tuesday ,your akan name is " + boy[2]);
-  }
+    alert("Born on Tuesday ,your akan name is " + boy[2]);
+    }
   else if (Math.ceil(result) === 3 && gender === 'male') {
-  alert("Born on Wednesday ,your akan name is " + boy[3]);
-  }
+    alert("Born on Wednesday ,your akan name is " + boy[3]);
+    }
   else if (Math.ceil(result) === 4 && gender === 'male') {  
-  alert("Born on Thursday,your akan name is " + boy[4]);
-  }
+    alert("Born on Thursday,your akan name is " + boy[4]);
+    }
   else if (Math.ceil(result) === 5 && gender === 'male') {  
-  alert("Born on Friday,your akan name is " + boy[5]);
-  }
+    alert("Born on Friday,your akan name is " + boy[5]);
+    }
   else if (Math.ceil(result) === 6 && gender === 'male') {  
-  alert("Born on Saturday,your akan name is " + boy[6]);
-  }
+    alert("Born on Saturday,your akan name is " + boy[6]);
+    }
   else if (Math.ceil(result) === 0 && gender === 'male') {
-  alert("Born on Sunday,your akan name is " + boy[0]);
-  }
+    alert("Born on Sunday,your akan name is " + boy[0]);
+    }
   else if (Math.ceil(result) === 1 && gender === 'female') {  
-  alert("Born on monday ,your akan name is " + girl[1]);
-  }
+    alert("Born on monday ,your akan name is " + girl[1]);
+      }
   else if (Math.ceil(result) === 2 && gender === 'female') {  
-  alert("Born on Tuesday,your akan name is " + girl[2]);
-  }
+    alert("Born on Tuesday,your akan name is " + girl[2]);
+    }
   else if (Math.ceil(result) === 3 && gender === 'female') {  
-  alert("Born on Wednesday,your akan name is " + girl[3]);
-  }
+    alert("Born on Wednesday,your akan name is " + girl[3]);
+    }
   else if (Math.ceil(result) === 4 && gender === 'female') {  
-  alert("Born on Thursday,your akan name is " + girl[4]);
-  }
+    alert("Born on Thursday,your akan name is " + girl[4]);
+    }
   else if (Math.ceil(result) === 5 && gender === 'female') {  
-  alert("Born on Friday,your akan name is " + girl[5]);
-  }
+    alert("Born on Friday,your akan name is " + girl[5]);
+    }
   else if (Math.ceil(result) === 6 && gender === 'female') {
-  alert("Born on Saturday,your akan name is " + girl[6]);
-  }
+    alert("Born on Saturday,your akan name is " + girl[6]);
+    }
   else if (Math.ceil(result) === 0 && gender === 'female') {
-  alert("Born on Sunday,your akan name is " + girl[0]);
-  }
+    alert("Born on Sunday,your akan name is " + girl[0]);
+    }
   }
